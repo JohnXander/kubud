d95889cc20a5466f2f2e760a6e956c38332c2669
John Xander <johnblox5@gmail.com>
2022-11-24 00:23:50 +0100

    remove seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,25 +2,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.user.createMany({
-        data: [
-        {
-            username: 'John',
-            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg'
-        },
-        {
-          username: 'Xander',
-          img: 'https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg'
-        },
-        {
-          username: 'Blox',
-          img: 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/06/cat-217679.jpg?h=c4ed616d&itok=3qHaqQ56'
-        },
-        ]
-    })
+    // seed data removed
 }
-
-// change
 
 main()
   .then(async () => {
