5e5de211934f83710d8a9c8bda6438203dca5891
John Xander <johnblox5@gmail.com>
2022-11-24 16:15:11 +0100

    fix bug attempt 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,8 @@ export default function IndexPage() {
     return <div>Loading...</div>;
   }
 
+  //why?
+
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center text-white bg-gray-800'>
       <button
