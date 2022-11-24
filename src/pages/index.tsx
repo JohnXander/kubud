c83f29f594b9c30bb1295cb20fc79cf80dd0153b
John Xander <johnblox5@gmail.com>
2022-11-24 15:48:52 +0100

    remove greeting function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
 import { trpc } from '../utils/trpc';
 
 export default function IndexPage() {
-  const hello = trpc.hello.useQuery({ text: 'client' });
-
   const createUserMutation = trpc.createUser.useMutation();
 
   const createNewUser = () => {
-    createUserMutation.mutate({username: 'Test 2'})
+    createUserMutation.mutate({username: 'Test 3'})
   }
   
-  if (!hello.data) {
+  if (!createUserMutation) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center text-white bg-gray-800'>
-      <p className='text-3xl'>{hello.data.greeting}</p>
       <button
         className='text-center text-white border rounded p-2.5 hover:bg-white hover:text-gray-800'
         onClick={() => createNewUser()}
